73ecca5178100542242e632711a5b5fcd4c37334
Vasilii Besschetnov <vbesschetnov@yandex.ru>
2018-02-08 11:37:22 -0800

    2 simple components are added

--- src/Map.jsx
+++ src/Map.jsx
@@ -3,6 +3,18 @@ import * as d3 from 'd3';
 import arteries from "$src/maps/arteries.json";
 import * as styles from "$src/styles/main.scss";
 
+const Path = ({
+    pathGenerator,
+    feature
+}) => {
+    return (
+        <path
+         d={pathGenerator(feature)}
+         className={styles.artery}
+         />
+    );
+}
+
 const Map = ({ }) => {
     // create a projection
     const projection = d3.geoEquirectangular();
@@ -11,6 +23,10 @@ const Map = ({ }) => {
     const path = d3.geoPath().projection(projection);
     return (
         <svg width={960} height={480} className={styles}>
+            {arteries.features.map((a, i) => <Path
+                                                 key={i}
+                                                 feature={a}
+                                                 pathGenerator={path} />)}
         </svg>
     );
 }
--- src/index.js
+++ src/index.js
@@ -16,26 +16,15 @@ ReactDOM.render(<Map />, document.getElementById('root'))
 
 
 
-var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    //.call(zoomer);
-
-let scale = 1;
-console.log(scale)
-
-
 // create a path that is bound to the projection
-var path = d3.geoPath()
-    .projection(projection);
+//var path = d3.geoPath()
+//    .projection(projection);
 
-projection.fitExtent([[20, 20], [920, 440]], arteries);
-
-svg.selectAll("."+ styles.artery)
-    .data(arteries.features)
-    .enter().append("path")
-    .attr("class", styles.artery)
-    .attr("d", path)
+//svg.selectAll("."+ styles.artery)
+//    .data(arteries.features)
+//    .enter().append("path")
+//    .attr("class", styles.artery)
+//    .attr("d", path)
 
 //svg.selectAll("."+ styles.neighborhood)
 //    .data(neighborhoods.features)
@@ -49,105 +38,11 @@ svg.selectAll("."+ styles.artery)
 //    .attr("class", styles.street)
 //    .attr("d", path)
 
-svg.selectAll("."+ styles.freeway)
-    .data(freeways.features)
-    .enter().append("path")
-    .attr("class", styles.freeway)
-    .attr("d", path)
-
-
-window.center = function () {
-    console.log(arteries);
-    let a = arteries;
- var bounds = path.bounds(arteries),
-     currTranslate = projection.translate(),
-      dx = bounds[1][0] - bounds[0][0],
-      dy = bounds[1][1] - bounds[0][1],
-      x = bounds[0][0],
-      y = bounds[0][1],
-      translate;
-    
-    translate = [currTranslate[0]- x, currTranslate[1] - y]
-  projection
-  .translate(translate);
-    
-    svg.selectAll("."+ styles.artery).attr("d", path)
-}
-//center();
-
-
-    function log() {
-    var bounds = path.bounds(arteries),
-      dx = bounds[1][0] - bounds[0][0],
-      dy = bounds[1][1] - bounds[0][1],
-      minX = Math.min(bounds[0][0], bounds[1][0]),
-      minY = Math.min(bounds[0][1], bounds[1][1]),
-      maxX = Math.max(bounds[0][0], bounds[1][0]),
-      maxY = Math.max(bounds[0][1], bounds[1][1]);
-    
-    console.table({
-        scale,
-        dx,
-        dy,
-        minX,
-        minY,
-        maxX,
-        maxY,
-    })
-    }
-
-function zoom(e, addition) {
-    console.log(arguments)
-    log();
-    addition = addition || 10000;
-    scale += addition;
-    projection.scale(scale)
-    
-    center();
-    svg.selectAll("."+ styles.artery).attr("d", path)
-    log();
-    console.log(d3.event);
-}
-
-function zoomToView() {
-    var bounds = path.bounds(arteries),
-        currScale = projection.scale(),
-        dx = bounds[1][0] - bounds[0][0],
-        dy = bounds[1][1] - bounds[0][1];
-    
-    scale = 0.9 / Math.max(dx / width, dy / height);
-    
-    log();
-    projection.scale(scale);
-    
-    svg.selectAll("."+ styles.artery).attr("d", path)
-    log();
-}
-
-Object.assign(window, {
-    projection,
-    path,
-    svg,
-    center,
-    styles,
-    arteries,
-    zoom,
-        log,
-    d3,
-    zoomToView
-});
-
-//-----------------------------
-
-
-
-
-window.test = projection;
-
-
-
-
-var graticule = d3.geoGraticule();
+//svg.selectAll("."+ styles.freeway)
+//    .data(freeways.features)
+//    .enter().append("path")
+//    .attr("class", styles.freeway)
+//    .attr("d", path)
 
 
 
