241ee34660da026a3ae833cb289ebecb9c3c21fc
Vasilii Besschetnov <vbesschetnov@yandex.ru>
2018-02-09 12:13:33 -0800

    styles were added

--- src/components/RouteList/RouteList.jsx
+++ src/components/RouteList/RouteList.jsx
@@ -2,20 +2,24 @@ import React from "react";
 import { connect } from "react-redux";
 import { getTagList } from "$src/reducers/reducers.js";
 import RouteItem from "./RouteItem.jsx";
+import * as styles from "./routeList.scss";
 
 const RouteList = ({
     tagList,
     onReload
 }) => {
-    
     return (
-        <div>
-            {tagList.map(tag =>
-                <RouteItem
-                    key={tag} tag={tag}
-                    />)}
-            <button onClick={onReload}>Refresh</button>
-        </div>
+        <section className={styles.routeList}>
+            <div className={styles.list}>
+                {tagList.map(tag =>
+                    <RouteItem
+                        key={tag} tag={tag}
+                        />)}
+            </div>
+            <div className={styles.footer}>
+                <button onClick={onReload}>Refresh</button>
+            </div>
+        </section>
     );
 };
 
