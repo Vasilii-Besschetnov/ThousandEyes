815d195cc466bb09e0d3776ebb843a98ac30492d
Vasilii Besschetnov <vbesschetnov@yandex.ru>
2018-02-09 16:01:17 -0800

    RouteList loads data by itself

--- src/components/RouteList/RouteList.jsx
+++ src/components/RouteList/RouteList.jsx
@@ -3,26 +3,74 @@ import { connect } from "react-redux";
 import { getTagList } from "$src/reducers/reducers.js";
 import RouteItem from "./RouteItem.jsx";
 import * as styles from "./routeList.scss";
+import { routeInfosLoaded } from "$src/actions/actions.js";
+import { withAxios } from "react-axios";
 
-const RouteList = ({
-    tagList,
-    onReload
-}) => {
-    return (
-        <section className={styles.routeList}>
-            <div className={styles.list}>
-                {tagList.map(tag =>
-                    <RouteItem
-                        key={tag} tag={tag}
-                        />)}
-            </div>
-            <div className={styles.footer}>
-                <button onClick={onReload}>Refresh</button>
-            </div>
-        </section>
-    );
-};
 
-export default connect((state) => ({
+
+class RouteList extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.loadData = this.loadData.bind(this);
+    }
+    
+    componentDidMount() {
+        this.loadData();
+    }
+    
+    loadData() {        
+        if (this.isLoading) return;
+
+        this.isLoading = true;
+        const params = {
+            command: "routeList",
+            a: "sf-muni" // agency
+        };
+        
+        const {
+            axios,
+            routeInfosLoaded
+        } = this.props;
+        axios.get("", { params }).then((response) => {
+            this.isLoading = false;
+            const data = response && response.data;
+            if (data) {
+                if (data.route) {
+                    routeInfosLoaded(Array.from(data.route));
+                }
+            }
+        }, (err) => {
+            this.isLoading = false;
+            this.loadData();
+        });
+        
+    }
+    
+    render () {
+        const {
+            tagList,
+
+        } = this.props;
+        
+        return (
+            <section className={styles.routeList}>
+                <div className={styles.list}>
+                    {tagList.map(tag =>
+                        <RouteItem
+                            key={tag} tag={tag}
+                            />)}
+                </div>
+                <div className={styles.footer}>
+                    <button onClick={this.loadData}>Refresh</button>
+                </div>
+            </section>
+        );
+    }
+}
+
+export default withAxios(connect((state) => ({
     tagList: getTagList(state)
-}))(RouteList)
+}), {
+    routeInfosLoaded
+})(RouteList))
